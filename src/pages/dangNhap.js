4fb6f52da0f85b8c48344ebc2b9869f9d5a7ada4
daibede <xuandai300092@gmail.com>
2022-01-12 14:34:19 +0700

    updata dang nhap dang ky

--- src/pages/dangNhap.js
+++ src/pages/dangNhap.js
@@ -5,20 +5,50 @@ const DN = {
                 <h1>Trang Đăng Nhập</h1>
 
                 <form>
-                <label class="block">
-                  <span class="block text-sm font-medium text-gray-700">Username</span>
-                  <!-- Using form state modifers, the classes can be identical for every input -->
-                  <input type="text" value="tbone" disabled class="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400
-                    focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
-                    disabled:bg-gray-50 disabled:text-gray-500 disabled:border-gray-200 disabled:shadow-none
-                    invalid:border-pink-500 invalid:text-pink-600
-                    focus:invalid:border-pink-500 focus:invalid:ring-pink-500
-                  "/>
-                </label>
-                <!-- ... -->
-              </form>
-            </div>
-            
+                <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+                <div class="max-w-md w-full space-y-8">
+                  <form class="mt-8 space-y-6" action="#" method="POST">
+                    <input type="hidden" name="remember" value="true">
+                    <div class="rounded-md shadow-sm -space-y-px">
+                      <div>
+                        <label for="email-address" class="sr-only">Email address</label>
+                        <input id="email-address" name="email" type="email" autocomplete="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address">
+                      </div>
+                      <div>
+                        <label for="password" class="sr-only">Password</label>
+                        <input id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password">
+                      </div>
+                    </div>
+              
+                    <div class="flex items-center justify-between">
+                      <div class="flex items-center">
+                        <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded">
+                        <label for="remember-me" class="ml-2 block text-sm text-gray-900">
+                          Remember me
+                        </label>
+                      </div>
+              
+                      <div class="text-sm">
+                        <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">
+                          Forgot your password?
+                        </a>
+                      </div>
+                    </div>
+              
+                    <div>
+                      <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        <span class="absolute left-0 inset-y-0 flex items-center pl-3">
+                          <!-- Heroicon name: solid/lock-closed -->
+                          <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                            <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
+                          </svg>
+                        </span>
+                        Sign in
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </div          
         `;
     },
 };
--- src/pages/home.js
+++ src/pages/home.js
@@ -12,9 +12,9 @@ const HomePage = {
                 <div class="grid grid-cols-3 gap-8">
                     ${data.map((post) => `
                         <div class="border p-4">
-                            <a href="${post.id}>
-                                <img src="/news/${post.img}" alt="" />
-                            </a>
+                        <a href="/news/${post.id}">
+                        <img src="${post.img}" alt="" />
+                          </a>
                             <h3 class="my-3"><a href="/news/${post.id}" class="font-semibold text-lg text-orange-500">${post.title}</a></h3>
                             <p>${post.desc}</p>
                         </div>
