c75f51d23ab0acaf5693cd031a6ce519d4a7469d
daibede <xuandai300092@gmail.com>
2022-01-14 11:01:23 +0700

    updata image new edit

--- src/admin/news/edit-new.js
+++ src/admin/news/edit-new.js
@@ -21,7 +21,7 @@ const editNews = {
                     class="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-gray-700">
                     Hình Ảnh Hiện Tại: ${router.img}
                 </span>
-                
+                <img src="${router.img}" alt="">
                 <input type="file" name="hinhanh"
                     class="mt-1 px-3 py-2 bg-white border shadow-sm border-gray-300 placeholder-gray-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
                     value=""/>
