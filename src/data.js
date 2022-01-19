66341d0214dee8843d75d8172d7fc84683a10f75
daibede <xuandai300092@gmail.com>
2022-01-19 07:36:08 +0700

    upload vercel

--- src/data.js
+++ src/data.js
@@ -1,9 +1,10 @@
 const data = [
     {
         createdAt: "2022-01-10T07:26:52.549Z",
-        title: "Annie Stroman",
+        title: "Vinh danh 295 học sinh FPT Polytecnic học kỳ 2018",
         img: "http://placeimg.com/640/480/fashion",
-        desc: "Andy shoes are designed to keeping in mind durability as well as trends, the most stylish range of shoes & sandals",
+        desc: "Tối ngày 16/06/2018, Cao đẳng FPT Polytechnic Hà Nội đã tổ chức Lễ tôn vinh “Ong vàng Poly”, tuyên dương 295 cá nhân và 05 CLB có thành tích xuất sắc trong học tập",
+        detail: "Tối ngày 16/06/2018, Cao đẳng FPT Polytechnic Hà Nội đã tổ chức Lễ tôn vinh “Ong vàng Poly”, tuyên dương 295 cá nhân và 05 CLB có thành tích xuất sắc trong học tập. Sáng 15/06 vừa qua, Phòng Quan hệ doanh nghiệp Cao đẳng FPT Polytecnic Hà Nội đã tổ chức Talkshow trải nghiệm khách hàng xu thế mới để phát triển doanh nghiệp phát triển bền vững. Với triết lý đào tạo “Thực học – Thực nghiêp”, Cao đẳng FPT Polytechnic Đà Nẵng thường xuyên tổ chức cho sinh viên tham quan các đơn vị doanh nghiệp.",
         id: "1",
     },
     {
