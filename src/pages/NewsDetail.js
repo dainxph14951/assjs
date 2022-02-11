03cc5253f2b4edc7a8c5b32e131e46b98144addb
daibede <xuandai300092@gmail.com>
2022-02-11 18:14:48 +0700

    add products

--- src/pages/NewsDetail.js
+++ src/pages/NewsDetail.js
@@ -1,20 +1,20 @@
-import data from "../data";
+import { get } from "../api/posts";
 import Header from "../components/header";
 import Banner from "../components/banner";
 import Footer from "../components/footer";
 
 const NewsDetail = {
-    render(id) {
-        const found = data.find((element) => element.id === id);
+    async  render(id) {
+        const { data } = await get(id);
 
         return `<div class="max-w-5xl mx-auto">
         ${Header.render()}
         <div class="banner">
         ${Banner.render()}
          </div>
-            <h1>${found.title}</h1>
-            <img src="${found.img}" />
-            <p>${found.desc}</p>
+            <h1>${data.title}</h1>
+            <img src="${data.img}" />
+            <p>${data.desc}</p>
         </div>
         ${Footer.render()}`;
     },
--- src/pages/admin/news/add.js
+++ src/pages/admin/news/add.js
@@ -1,3 +1,4 @@
+import { add } from "../../../api/posts";
 import NavAdmin from "../../../components/NavAdmin";
 
 const AddNewsPage = {
@@ -17,7 +18,12 @@ const AddNewsPage = {
                 <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
                     <div class="px-4 py-6 sm:px-0">
                         <div class="border-4 border-dashed border-gray-200 rounded-lg h-96">
-                            form
+                        <form action="" id="form-add-post">
+                        <input type="text" placeholder="Tieu Đề Bài Viết" class="border border-black" id="title-post"></br>
+                        <input type="text" placeholder="Ảnh Bài Viết" class="border border-black" id="img-post"></br>
+                        <textarea name="" id="desc-post" cols="40" rows="5" class="border border-black"></textarea>
+                        <button>Thêm</button>
+                      </form>
                         </div>
                     </div>
                 </div>
@@ -25,6 +31,21 @@ const AddNewsPage = {
         </div>
     
                     `;
+    },
+    afterRender() {
+        const formAdd = document.querySelector("#form-add-post");
+        formAdd.addEventListener("submit", (e) => {
+            e.preventDefault();
+            const postFake = {
+                title: document.querySelector("#title-post").value,
+                img: document.querySelector("#img-post").value,
+                desc: document.querySelector("#desc-post").value,
+            };
+            add(postFake)
+                .then((result) => console.log(result.data))
+                .catch((error) => console.log(error));
+            // axios.post("https://5e79b4b817314d00161333da.mockapi.io/posts", postFake);
+        });
     },
 };
 export default AddNewsPage;
