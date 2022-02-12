a7a9129e985ddbb79e6b42a215147fa59386554d
daibede <xuandai300092@gmail.com>
2022-02-12 20:16:46 +0700

    db json

--- src/pages/dangNhap.js
+++ src/pages/dangNhap.js
@@ -1,31 +1,39 @@
+import toastr from "toastr";
 import { signin } from "../api/user";
 import { $ } from "../utils/selector";
+import "toastr/build/toastr.min.css";
 
 const DN = {
     render() {
         return `
-        <form id="formSignin">
-            <input type="email" class="border border-black" id="email" placeholder="Your  email"/>
-            <input type="password" class="border border-black" id="password" placeholder="Your  password"/>
-            <button>Đăng nhập</button>
-        </form>
-    `;
+            <form id="formSignin">
+                <input type="email" class="border border-black" id="email" placeholder="Your  email"/>
+                <input type="password" class="border border-black" id="password" placeholder="Your  password"/>
+                <button>Đăng nhập</button>
+            </form>
+        `;
     },
     afterRender() {
         $("#formSignin").addEventListener("submit", async (e) => {
             e.preventDefault();
             try {
-            // call api, nếu đăng nhập thành công sẽ trả về object data
+                // call api, nếu đăng nhập thành công sẽ trả về object data
                 const { data } = await signin({
                     email: $("#email").value,
                     password: $("#password").value,
                 });
-                // console.log('data', data);
-                alert("Đăng nhập thành công");
-                document.location.href = "/#/";
+                localStorage.setItem("user", JSON.stringify(data.user));
+                toastr.success("Đăng nhập thành công, chuyển trang sau 2s");
+                setTimeout(() => {
+                    if (data.user.id === 1) {
+                        document.location.href = "/#/admin/news";
+                    } else {
+                        document.location.href = "/#/";
+                    }
+                }, 2000);
             } catch (error) {
-            // nếu lỗi thì trả về object chứa lỗi error.response.data
-                alert(error.response.data);
+                // nếu lỗi thì trả về object chứa lỗi error.response.data
+                toastr.success(error.response.data);
                 $("#formSignin").reset();
             }
         });
